e628999f6e65cc48e1c50467152c605536fd85fe
Chen Mualem <chenmu10@gmail.com>
2023-06-18 22:22:09 +0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,6 @@
+import NoImage from '../assets/no-image-placeholder-6f3882e0.webp';
 export const getCroppedImageUrl = (url: string) => {
+  if (!url) return NoImage;
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
