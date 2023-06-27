b019725273982373dac840d34ad3a734c98dbd24
Chen Mualem <chenmu10@gmail.com>
2023-06-27 21:17:30 +0300

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
 import GameDetailPage from './pages/GameDetailPage';
 import Homepage from './pages/Homepage';
 import Layout from './pages/Layout';
@@ -7,11 +8,14 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    children: [{ index: true, element: <Homepage /> }],
-  },
-  {
-    path: 'game/:id',
-    element: <GameDetailPage />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Homepage /> },
+      {
+        path: 'game/:id',
+        element: <GameDetailPage />,
+      },
+    ],
   },
 ]);
 
